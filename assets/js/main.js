67003fa7baf89a337cdb49248a50fac9bdb6bd27
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2023-12-20 21:02:07 +0700

    update code

---
+++ assets/js/main.js
@@ -0,0 +1,149 @@
+/*
+ - LocalStorage bao gom 2 phan: 
+   + key: Do minh dat;
+   + Value: Gia tri cua thong tin minh muon luu vao
+
+  - Khai bao 1 gia tri localStorage cu phap:
+    localStorage.setItem("KEY", "Value");
+
+  - Lay 1 gia tu localStorage de su dung:
+   localStorage.getItem("KEY");
+
+   - Xoa 1 gia tri localStorage
+    localStorage.removeItem("KEY");
+
+  - Xoa nhieu gia tri localStorage
+  localStorage.clear();
+
+*/
+
+// Khai bao 1 localStorage co gia tri web38
+// localStorage.setItem("CLASS", "Web38");
+
+// Lay gia tri localStorage voi Key "CLASS"
+// let CLASS = localStorage.getItem("CLASS");
+
+
+// Xoa 1 gia tri localStorage voi Key "CLASS"
+// localStorage.removeItem("CLASS");
+
+
+
+/*
+  Thuc hanh ung dung Todolist
+  - Database minh can luu tru no nhu nao ?
+    [
+      {
+        "name": "Cong viec 1",
+        "completed": "Hoan thanh"
+      },
+      {
+        "name": "Cong viec 2",
+        "completed": "chua hoan thanh"
+      }
+    ]
+*/
+
+// B1: Truy cap phan tu
+let formElement = document.querySelector("#form");
+let inputElement = document.querySelector("#input");
+let ulElement = document.querySelector("#todos");
+
+// B2: Thiet ke DB && Thuc hien chuong trinh -- Mockup
+// let DB = [
+//   {
+//     "name": "Cong viec 1",
+//     "completed": true
+//   },
+//   {
+//     "name": "Cong viec 2",
+//     "completed": false
+//   }
+// ];
+
+// Luu vao localStorage
+// localStorage.setItem("TASK", JSON.stringify(DB));
+
+
+// B3: Hien thi du lieu ra Giao dien nguoi dung
+
+// B3.1: Get du lieu tu Database
+let listTodo = JSON.parse(localStorage.getItem("TASK"));
+
+// B3.2: Kiem tra listTodo ton tai hay khong
+
+let updateStorage = () => {
+
+  let newLiElement = document.querySelectorAll('li');
+
+  let todo = [];
+  newLiElement.forEach((item)=>{
+    // console.log(item);
+    todo.push({
+      "name": item.innerHTML,
+      "completed": item.classList.contains("completed")
+    });
+  });
+
+  console.log(todo);
+
+  localStorage.setItem("TASK", JSON.stringify(todo));
+
+}
+
+let showTodo = (list) => {
+  let nameTodo = inputElement.value; //value no cu phap
+  if (list) {
+    nameTodo = list.name; //in ra ten cong viec
+  }
+
+  if (nameTodo) {
+    let liElement = document.createElement("li"); //Tao the <li></li>
+
+    // Check CV hoan thanh hay chua -> add class completed ?
+    if(list && list.completed == true) { //list va list.name phai tai
+      // Cu phap them 1 the tu js vao trong css: classList.add
+      liElement.classList.add("completed");
+    }
+
+    liElement.innerHTML = nameTodo; //gan li vs ten cong viec
+
+    // Edit Trang thai cong viec
+    liElement.addEventListener("click", ()=>{
+      liElement.classList.toggle("completed"); // classList.toggle() co san
+      updateStorage();
+    });
+
+    // Remove cong viec duoc chon
+    liElement.addEventListener("contextmenu", (event)=>{
+      event.preventDefault();
+      liElement.remove(); //remove() co san  js
+      updateStorage();
+    });
+
+
+
+    ulElement.appendChild(liElement); //in ra
+    inputElement.value = '';
+    updateStorage();
+
+  }
+
+
+}
+
+// B3.2.1: ton ai
+if (listTodo) {
+  listTodo.forEach((value, index) => {
+    showTodo(value);
+  });
+}
+
+// B3.2.2: Khong ton tai
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault(); //default
+
+  showTodo();
+});
+
+
